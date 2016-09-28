ac3d743f18214d2fc92e75a454203ec05f7ac359
pengkobe <526337223@qq.com>
2016-09-28 09:29:43 +0800

    start to devise business file structure

--- www/dash/js/dash.js
+++ www/dash/js/dash.js
@@ -1,2 +1,17 @@
 angular.module('dash', [])
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+    // 构建消息UI模板
+    $scope.buildTplUrl = function (type) {
+      var tplUrl;
+      switch (type) {
+        case 'industry':
+          tplUrl = 'industry'; break;
+        case 'medical':
+          tplUrl = 'medical'; break;
+        case 'aircondition':
+          tplUrl = 'aircondition';break;
+        default:// TODO：隐藏业务tab
+      }
+      return 'dash/business/' + tplUrl + '/' + tplUrl + '.html';
+    };
+})
