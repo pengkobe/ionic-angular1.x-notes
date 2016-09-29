8d495fd34affc7b2d44f02b3cba824972f2d9cf7
pengkobe <526337223@qq.com>
2016-09-29 09:43:38 +0800

    test using complex directive to build business

--- www/dash/js/dash.js
+++ www/dash/js/dash.js
@@ -5,17 +5,32 @@ angular.module('dash', [])
       var tplUrl;
       switch (type) {
         case 'industry':
-          tplUrl = 'industry'; 
+          tplUrl = 'industry';
           break;
         case 'medical':
-          tplUrl = 'medical'; 
+          tplUrl = 'medical';
           break;
         case 'aircondition':
-          tplUrl = 'aircondition'; 
+          tplUrl = 'aircondition';
           break;
         default:
-           // TODO：隐藏业务tab
+        // TODO：隐藏业务tab
       }
       return 'dash/business/' + tplUrl + '/' + tplUrl + '.html';
     };
+
+    $scope.expanders = [
+      {
+        title: 'Click me to expand',
+        text: 'Hi there folks, I am the content that was hidden but is now shown.'
+      },
+      {
+        title: 'Click this',
+        text: 'I am even better text than you have seen previously'
+      },
+      {
+        title: 'Test',
+        text: 'test'
+      }];
+
   })
